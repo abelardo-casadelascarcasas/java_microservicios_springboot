ae4bacb4cb5f204f1983a73da9895f8f210fe78f
Abelardo León González <abelardoleon@lacasadelascarcasas.es>
2024-10-17 11:32:06 +0200

    Before 63

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -132,7 +132,7 @@ public class CustomerRestController {
     public ResponseEntity<?> post(@RequestBody Customer input) {
         input.getProducts().forEach(p -> p.setCustomer(input));
         Customer save = customerRepository.save(input);
-        return ResponseEntity.ok(save);
+        return ResponseEntity.status(HttpStatus.CREATED).body(save);
     }
     
     @DeleteMapping("/{id}")
